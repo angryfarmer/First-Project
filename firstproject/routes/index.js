df72ada3f52f81fb52225194483471a47ed5adab
angryfarmer <ericyu922@gmail.com>
2015-10-31 20:43:23 -0700

    new search bar added

--- firstproject/routes/index.js
+++ firstproject/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Easy' });
+});
+
+router.get('/a', function(req, res) {
+  res.send('no no no');
+});
+
+router.get('/:text', function(req, res) {
+  res.send(req.params.text);
 });
 
 module.exports = router;
